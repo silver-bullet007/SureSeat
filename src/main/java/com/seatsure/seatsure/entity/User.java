de6f4ac4dc472550655aeb4f31dda4949614c20b
Lokesh <lokeshsundarav@gmail.com>
2026-08-06 19:39:47 +0530

    Adding the config yaml and also creating an initial User Entity

---
+++ src/main/java/com/seatsure/seatsure/entity/User.java
@@ -0,0 +1,42 @@
+package com.seatsure.seatsure.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users") // "user" is a reserved SQL keyword in Postgres, so we rename the table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password; // we'll hash this properly once Spring Security enters in Stage 4
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public enum Role {
+        USER, ORGANIZER, ADMIN
+    }
+}
