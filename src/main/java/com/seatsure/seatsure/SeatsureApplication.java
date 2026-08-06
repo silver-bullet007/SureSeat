e69274347f68fd44c52b622295249d12263cc185
Lokesh <lokeshsundarav@gmail.com>
2026-08-06 16:35:16 +0530

    Initial Project Structure

---
+++ src/main/java/com/seatsure/seatsure/SeatsureApplication.java
@@ -0,0 +1,13 @@
+package com.seatsure.seatsure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatsureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatsureApplication.class, args);
+	}
+
+}
